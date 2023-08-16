09095549c059442804a7be1b8dd0a1688651f107
bando <codenamenui@gmail.com>
2023-08-16 21:03:23 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Initializing Pygame
+pygame.init()
+
+# Initializing Pygame Essential Variable
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+FPS = 60
+
+if __name__ == "__main__":
